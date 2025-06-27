22872463fefffae8a1bb1378828387afeccd37ac
Eldeeb <ziad.shararaa@gmail.com>
2025-06-27 07:19:53 +0300

    Update addresses validator

--- utils/validators/addressesValidator.js
+++ utils/validators/addressesValidator.js
@@ -1,16 +1,39 @@
 import { check } from 'express-validator';
+import { User } from '../../models/userModel.js';
 import { validatorMiddleware } from '../../middlewares/validatorMiddleware.js';
 
 export const createAddressValidator = [
+  check('alias')
+    .notEmpty()
+    .withMessage('Alias is required')
+    .custom(async (val, { req }) => {
+      const user = await User.findById(req.user._id);
+      if (!user) {
+        throw new Error('User not found');
+      }
+
+      const isDuplicate = user.addresses.some(
+        address => address.alias.toLowerCase() === val.toLowerCase(),
+      );
+
+      if (isDuplicate) {
+        throw new Error('Address alias already exists!');
+      }
+
+      return true;
+    }),
+
   check('phone')
-    .optional()
+    .notEmpty()
+    .withMessage('Phone is required')
     .isMobilePhone(['ar-EG', 'ar-SA'])
     .withMessage(
       'Invalid phone number, only accepted Egy and SA phone numbers!',
     ),
 
   check('postalCode')
-    .optional()
+    .notEmpty()
+    .withMessage('Postal Code is required')
     .isPostalCode('any')
     .withMessage('Invalid postal code!'),
 
