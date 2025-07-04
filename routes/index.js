8b22fddea869088ca85a94efb9df2db75cd6f662
Eldeeb <ziad.shararaa@gmail.com>
2025-07-05 00:22:54 +0300

    Edit Routes

--- routes/index.js
+++ routes/index.js
@@ -17,7 +17,7 @@ const mountRoutes = (app) => {
   app.use('/api/v1/brands', brandRoute);
   app.use('/api/v1/products', productRoute);
   app.use('/api/v1/users', userRoute);
-  app.use('/auth', authRoute);
+  app.use('/api/v1/auth', authRoute);
   app.use('/api/v1/reviews', reviewRoute);
   app.use('/api/v1/wishlist', wishlistRoute);
   app.use('/api/v1/addresses', addressesRoute);
